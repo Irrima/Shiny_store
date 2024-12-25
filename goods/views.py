e83e1667aa48d89e52403571ce9b694fa3812dad
Irrima <irina.k.maltseva@gmail.com>
2024-12-25 16:49:01 +0500

    second option pagination

--- goods/views.py
+++ goods/views.py
@@ -5,13 +5,15 @@ from goods.models import Categories, Products
 
 def catalog(request, category_slug, page=1):
 
+    page = request.GET.get('page', 1)
+
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products, category__slug=category_slug)
 
     paginator = Paginator(goods, 4)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     categories = Categories.objects.all()
 
